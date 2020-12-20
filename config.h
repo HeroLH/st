5528d483001959bdbff4c87f54dda0caa42aa0b9
Herolh <you@example.com>
2020-12-21 06:20:42 +0800

    patch: scrollback
    允许往前滚动历史记录,
    修改上下翻滚一行按键为 `C-j` 与 `C-h`
    修改上下翻滚一页按键为 `C-u` 与 `C-e`

--- config.h
+++ config.h
@@ -212,6 +212,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,          XK_j,           kscrollup,      {.i =  1} },
+	{ ControlMask,          XK_k,           kscrolldown,    {.i =  1} },
+	{ ControlMask,          XK_u,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_e,           kscrolldown,    {.i = -1} },
 };
 
 /*
